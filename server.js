e353fee12a5ef1d8d570fb3645fd975cce5ff474
Aaron Imbrock <aaron@sizza.net>
2023-04-22 00:08:59 -0600

    Updated README, updated dotenv, created modules with starter code

--- server.js
+++ server.js
@@ -1,39 +1,33 @@
 'use strict';
 
-// (*******REQUIRE****** (like import but for the backend)
-//
 const express = require('express');
 require('dotenv').config();
 const cors = require('cors');
+// const axios = require('axios');
+const getPhotos = require('./modules/photos');
 
-// *** app === server - Need to call Express to create the server
 const app = express();
-// *** MIDDLEWARE *** allow anoyone to hit my server
+
 app.use(cors());
 
 const PORT = process.env.PORT || 3002;
 
+app.listen(PORT, ()=> console.log(`Yay we are up on port ${PORT}`));
 
-app.listen(PORT, () => console.log(`We are up on port ${PORT}`));
-
-// **** ENDPOINTS ****
-// *** 1ST ARG - ENDPOINT URL
-// *** 2ND ARG - CALLBACK WHICH WILL EXECUTE WHEN THAT ENDPOINT IS HIT
-//		** 2 parameters: request, response
-app.get('/', (req, res) => {
-	res.status(200).send('Welcome to My Server');
+app.get('/', (request, response)=>{
+  response.status(200).send('Welcome to my server!');
 });
 
-app.get('/hello', (req, res) => {
-	let firstName = request.query.firstName;
-	let lastName = request.query.lastName;
+app.get('/photos', getPhotos);
 
-	console.log(request.queery);
-	
-	response.status(200).send(`Hello ${firstName} ${lastName}, welcome to my server!`);
+
+app.get('*', (request, response) => {
+  response.status(404).send('This page does not exist');
 });
 
-// *** CATCH ALL ENDPOINT SHOULD BE THE LAST DEFINED ***
-app.get('*', (req, res) => {
-	res.status(404).send('This page does not exist.');
+
+// **** ERROR HANDLING - PLUG AND PLAY CODE FROM EXPRESS DOCS ****
+app.use((error, request, response, next) => {
+  console.log(error.message);
+  response.status(500).send(error.message);
 });
